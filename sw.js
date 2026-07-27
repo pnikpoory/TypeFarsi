70003bb13244a1b6d70b94d5f4a73a1cfb85419e
Arena Agent <arena-agent@users.noreply.github.com>
2026-07-27 20:54:30 +0000

    Add more iPadOS Persian fonts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='farsitype-ipados-v2';
+const CACHE='farsitype-ipados-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
