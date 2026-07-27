d4ea05ab4700f61dda3065373b368f5d6152d090
Arena Agent <arena-agent@users.noreply.github.com>
2026-07-27 20:40:04 +0000

    Deploy GitHub Pages build

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='farsitype-ipados-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
